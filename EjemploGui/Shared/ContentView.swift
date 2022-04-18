f022d7735291ef1be45a763276649f5d07092238
Ruben <rubenestebancorrlaes@gmail.com>
2022-04-18 16:31:14 -0700

    feat: Añadido Text

--- EjemploGui/Shared/ContentView.swift
+++ EjemploGui/Shared/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Tortugas")
+        Text("Hello World")
             .padding()
+        Text("Login")
+        
         
     }
 }
