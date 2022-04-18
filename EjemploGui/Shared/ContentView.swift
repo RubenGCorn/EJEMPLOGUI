15d5b772d7c10d78a1595abe1234a25362b5dbd5
Ruben <rubenestebancorrlaes@gmail.com>
2022-04-18 16:21:06 -0700

    feat: He añadido un nuevo print

---
+++ EjemploGui/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alumno Swift on 18/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Tortugas")
+            .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
---
+++ EjemploGui/Shared/EjemploGuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploGuiApp.swift
+//  Shared
+//
+//  Created by Alumno Swift on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct EjemploGuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
